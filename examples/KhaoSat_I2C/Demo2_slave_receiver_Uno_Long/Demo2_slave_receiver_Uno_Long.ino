623e8239648cd48ee547146c81a9a59ef65c82cb
longhoney <long.711.sg@gmail.com>
2026-05-17 08:27:51 +0700

    Bien c bi ghi den len

--- examples/KhaoSat_I2C/Demo2_slave_receiver_Uno_Long/Demo2_slave_receiver_Uno_Long.ino
+++ examples/KhaoSat_I2C/Demo2_slave_receiver_Uno_Long/Demo2_slave_receiver_Uno_Long.ino
@@ -12,7 +12,7 @@
 
 #include <Wire.h>
 
-// char c = 0;
+String dataHex = "";
 
 void setup() {
   Wire.begin(64);                // join I2C bus with address #8 //dia chi I2C cua Slave dang thap phan
@@ -21,41 +21,69 @@ void setup() {
 }
 
 void loop() {
-  
-
   delay(100);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) { //dem Byte nhan ve
-  while (0 < Wire.available()) { // loop through all but the last //sanSang //chua 1 byte
-    char c = Wire.read(); // receive byte as a character
+  //dataHex = "";
+  while (Wire.available()) { // loop through all but the last //sanSang //chua 1 byte
+    byte c = Wire.read(); // receive byte as a character
     // Serial.print(c,HEX);         // print the character in Hex format
     // Serial.print(c);
     // Serial.print(" ");  //leave space
-    
-    if (Serial.available() > 0) {
-      switch (c) {
-        case '40107F1FFFFFFC1':   //Động cơ A quay thuận, tốc độ 50%
-          // digitalWrite(2, HIGH);
-          Serial.println("Động cơ A quay thuận, tốc độ 50%");
-          break;
-        case '40100142':    //Động cơ A ngừng quay thuận
-          // digitalWrite(3, HIGH);
-          Serial.println("Động cơ A ngừng quay thuận");
-          break;
-        case '4010FFFFFFFF040':   //Động cơ A quay nghịch, tốc độ 100%
-          // digitalWrite(4, HIGH);
-          Serial.println("Động cơ A quay nghịch, tốc độ 100%");
-          break;
-        case '40100041':    //Động cơ A ngừng quay nghịch
-          // digitalWrite(5, HIGH);
-          Serial.println("Động cơ A ngừng quay nghịch");
-          break;
-      }
-    }
+   
+    // In HEX và ghép chuỗi
+    dataHex += String(c, HEX);
   }
+  // Chuyển thành chữ HOA cho dễ so sánh
+  dataHex.toUpperCase();
+  
+  Serial.print("Nhan duoc: ");
+  Serial.println(dataHex);
+
+    // So sánh lệnh
+  if (dataHex == "40107F1C1") {
+    Serial.println("Động cơ A quay thuận, tốc độ 50%");
+  }
+
+  else if (dataHex == "40100142") {
+    Serial.println("Động cơ A ngừng quay thuận");
+  }
+
+  else if (dataHex == "4010FFFFFFFF040") {
+    Serial.println("Động cơ A quay nghịch, tốc độ 100%");
+  }
+
+  else if (dataHex == "40100041") {
+    Serial.println("Động cơ A ngừng quay nghịch");
+  }
+  else {
+    Serial.println("Lenh khong hop le");
+  }
+
+  /*if (Serial.available() > 0) {
+    switch (c) {
+      case '40107F1FFFFFFC1':   //Động cơ A quay thuận, tốc độ 50%
+        // digitalWrite(2, HIGH);
+        Serial.println("Động cơ A quay thuận, tốc độ 50%");
+        break;
+      case '40100142':    //Động cơ A ngừng quay thuận
+        // digitalWrite(3, HIGH);
+        Serial.println("Động cơ A ngừng quay thuận");
+        break;
+      case '4010FFFFFFFF040':   //Động cơ A quay nghịch, tốc độ 100%
+        // digitalWrite(4, HIGH);
+        Serial.println("Động cơ A quay nghịch, tốc độ 100%");
+        break;
+      case '40100041':    //Động cơ A ngừng quay nghịch
+        // digitalWrite(5, HIGH);
+        Serial.println("Động cơ A ngừng quay nghịch");
+        break;
+    }
+  }*/
+  
   // int x = Wire.read();    // receive byte as an integer
   // Serial.print("\t");
   // Serial.println(x,HEX);         // print the integer
